08a091a7b314a693aa1582d0c73fec3930dd73b4
Arda Tümay <arda.tumay@tedu.edu.tr>
2020-04-17 23:38:51 +0300

    last commit

--- ImageStitching.py
+++ ImageStitching.py
@@ -42,6 +42,8 @@ def getRandomElements(_list, size):
     
 def calculateHomography(points):
     
+    
+    # Create jacobian of homography matrix
     jacobian = np.matrix([0,0,0,0,0,0,0,0])
     for match in points:
         jacobian = np.vstack([jacobian , [match[0][0], match[0][1], 1, 0, 0, 0, -match[0][0] * match[1][0], -match[0][1] * match[1][0]]])
@@ -60,12 +62,6 @@ def calculateHomography(points):
     
     return H;   
 
-def matchFeaturesBuiltin(kp1, kp2, desc1, desc2):
-    print("Matching Features...")
-    matcher = cv.BFMatcher(cv.NORM_L2, True)
-    matches = matcher.match(desc1, desc2)
-    return matches
-
 def matchFeatures(kpsDsc1, kpsDsc2, debug = False):
 
     print("FEATURE MATCHING STARTS")
@@ -77,6 +73,7 @@ def matchFeatures(kpsDsc1, kpsDsc2, debug = False):
     
     matches = []
     
+    # Find two different points that are closest points to the  soruce point
     i, j = 0, 0
     indexDesc2 =  0
     for descriptor1 in kpsDsc1["descriptors"]:
@@ -137,11 +134,8 @@ def calculateEuclidianDistance(pointPair, H):
     # predictedPoint2x = np.divide((np.multiply(H[0, 0], matches[0][0][0]) + np.multiply(H[0, 1] , matches[0][0][1]) + H[0, 2]) ,  (np.multiply(H[2, 0], matches[0][0][0]) + np.multiply(H[2, 1], matches[0][0][1]) + 1 ))
     # predictedPoint2y = np.divide((np.multiply(H[1, 0], matches[0][0][0]) + np.multiply(H[1, 1] , matches[0][0][1]) + H[1, 2]) ,  (np.multiply(H[2, 0], matches[0][0][0]) + np.multiply(H[2, 1], matches[0][0][1]) + 1 )) 
         
-    error = np.matrix([[p2[0]], [p2[1]]]) - np.array(predictedPoint2)
-    return np.linalg.norm(error)
-    
-    # error = cv.norm(p2, predictedPoint2 ,cv.NORM_L2);
-    # return error
+    error = cv.norm(p2, predictedPoint2 ,cv.NORM_L2);
+    return error
 
 
 # RANSAC BELOW:
@@ -154,16 +148,20 @@ def ransac(matches, distanceTreshold = 2.5, inlierTreshold = 0.6, debug = False)
     bestH = []
     for i in range(numOfIter):
         
+        # Get 4 random point matches
         pointPairs = getRandomElements(matches, 4)
         
+        # Estimate points based on these matches
         H = calculateHomography(pointPairs)
         
         inliers = []
         for match in matches:
+            # If threshold is lower than threshold, regard match as inlier
             distance = calculateEuclidianDistance(match, H)
             if(distance < distanceTreshold):
                 inliers.append(match)
         
+        # Update inlier and homography
         if len(inliers) > len(bestInliers):
             bestH = H
             bestInliers = inliers
@@ -171,11 +169,13 @@ def ransac(matches, distanceTreshold = 2.5, inlierTreshold = 0.6, debug = False)
         if debug:
             print("Found inliers --> " + str(len(inliers)) + " Max inliers --> " + str(len(bestInliers)))        
        
+        # If enoguh number of inliers are found, stop process
         if len(bestInliers) > (len(matches) * inlierTreshold):
             break
         
     print("Final homography --> \n" + str(bestH))
     
+    # Save homography in text file
     f = open('homography.txt', 'w')
     f.write("Final homography: \n" + str(bestH)+"\n")
     f.write("Final inliers count: " + str(len(bestInliers)))
@@ -185,8 +185,87 @@ def ransac(matches, distanceTreshold = 2.5, inlierTreshold = 0.6, debug = False)
 
 def warpImages(img1, img2, H):
     
+    # Create a new output image that concatenates the two images together
+    rows1 = img1.shape[0]
+    cols1 = img1.shape[1]
+    rows2 = img2.shape[0]
+    cols2 = img2.shape[1]
+
+    out = np.zeros((rows1 + rows2, cols1 + cols1 + cols2), dtype='uint8')
+
+    # create indices of the destination image and linearize them
+    h, w = img1.shape[:2]
+    indy, indx = np.indices((h, w), dtype=np.float32)
+    lin_homg_ind = np.array([indx.ravel(), indy.ravel(), np.ones_like(indx).ravel()])
+    
+    # warp the coordinates of src to those of true_dst
+    map_ind = H.dot(lin_homg_ind)
+    map_x, map_y = map_ind[:-1]/map_ind[-1]  # ensure homogeneity
+    map_x = map_x.reshape(h, w).astype(np.float32)
+    map_y = map_y.reshape(h, w).astype(np.float32)
+    
+    # remap pixels from image 1 to image 2
+    dst = cv.remap(img2, map_x, map_y, cv.INTER_LINEAR)
+    blended = cv.addWeighted(img1, 0.7, dst, 0.3, 0)
+    cv.imwrite('stitchedImage.png', blended)
+            
+    return out
+
+# Not working
+def doGaussianNewton(bestInliers, initialH):
+    print("GAUS NEWTON STARTS")
+    bestH = initialH
+    for i in range(15):
         
+        jacobian = np.matrix([0,0,0,0,0,0,0,0])
+        res = np.matrix([0])
+        # For all inliers estimate delta homography
+        for inlier in bestInliers:
+            
+            sensedPoint = inlier[1]
+            
+            predictedPoint = np.matmul(bestH, np.vstack([np.matrix(inlier[0]).T, [1]]))
+            predictedPoint = predictedPoint / predictedPoint[2]
+            predictedPoint = predictedPoint[0:2]
+            
+            # Contruct jacobian of homography matrix
+            D = 1/(bestH[2,0] * bestH[0,0] + bestH[2,1] * inlier[0][1] + 1)
+            jacobian = np.vstack([jacobian , np.multiply(D , [inlier[0][0], inlier[0][1], 1, 0, 0, 0, -inlier[0][0] * predictedPoint[0,0], -inlier[0][1] * predictedPoint[0,0]])])
+            jacobian = np.vstack([jacobian , np.multiply(D , [0, 0, 0, inlier[0][0], inlier[0][1], 1, -inlier[0][0] * predictedPoint[1,0], -inlier[0][1] * predictedPoint[1,0]])])
+                
+            
+            res = np.vstack([res , [inlier[1][0] - predictedPoint[0,0] ]])
+            res = np.vstack([res , [inlier[1][1] - predictedPoint[1,0] ]])
         
+            
+           
+            
+        jacobian = jacobian[1:, :]
+        res = res[1:, :]
+        # Calculate delta homogtaphy
+        deltaH = np.matmul(np.linalg.inv(np.matmul(jacobian.T, jacobian)), np.matmul(jacobian.T, res))
+        deltaH = np.vstack([deltaH, [1]])
+        deltaH = deltaH.reshape((3,3))
+         
+        # Find new homography
+        bestH = bestH - deltaH
+        
+        # Find error based on new homography
+        error = 0
+        for inlier in bestInliers:
+            
+            predictedPointForTest = np.matmul(bestH, np.vstack([np.matrix(inlier[0]).T, [1]]))
+            predictedPointForTest = predictedPointForTest / predictedPointForTest[2]
+            predictedPointForTest = predictedPointForTest[0:2]
+            
+            
+            errorDistance = cv.norm(np.array(inlier[1]), predictedPointForTest, cv.NORM_L2)
+            error += errorDistance 
+        print("Error --> " + str(error))
+        
+    return bestH
+
+def drawMatches(img1, img2, matches):
     # Create a new output image that concatenates the two images together
     rows1 = img1.shape[0]
     cols1 = img1.shape[1]
@@ -195,115 +274,54 @@ def warpImages(img1, img2, H):
 
     out = np.zeros((max([rows1,rows2]),cols1+cols2,3), dtype='uint8')
 
-    H = np.matrix([[ 1.15976269e+00, -2.34391473e-02, -6.46162755e+02],
-                   [2.83148807e-03,  1.12860047e+00,  1.38407135e+02],
-                   [1.52419846e-04,  1.37118880e-05,  1.00000000e+00]])
-    
-
-    out = cv.warpPerspective(img1, H, (img1.shape[1] + img2.shape[1], img1.shape[0] +500))
-    out[0:img2.shape[0], 400:  400 +img2.shape[1]] = img2
-
     # Place the first image to the left
-    # out[:rows1,:cols1,:] = np.dstack([img1])
+    out[:rows1,:cols1,:] = np.dstack([img1])
 
-    # Place the next image to the right of it
-    # out[:rows2,cols1:cols1+cols2,:] = np.dstack([img2])
-    
-    # rows = img1.shape[0]
-    # cols = img1.shape[1] 
-    
-    # for row in range(rows):
-    #     for col in range(cols):
-    #         point = np.matrix([row, col, 1])
-    #         predictedPoint = np.matmul(H, point.T)
-    #         predictedPoint = predictedPoint / predictedPoint[2]
-    #         predictedPoint = predictedPoint[0:2]
-    #         out[int(predictedPoint[0, 0]), int(predictedPoint[1, 0])] = img2[row, col]
-    
-    
+    # Place the second image to the right of it
+    out[:rows2,cols1:cols1+cols2,:] = np.dstack([img2])
+
+    # For each pair of points we have between both images
+    # draw circles, then connect a line between them
+    for mat in matches:
+
+        # x - columns, y - rows
+        (x1,y1) = mat[0]
+        (x2,y2) = mat[1]
+
+        cv.circle(out, (int(x1),int(y1)), 4, (255, 0, 0), 1)
+        cv.circle(out, (int(x2)+cols1,int(y2)), 4, (255, 0, 0), 1)
+
+        cv.line(out, (int(x1),int(y1)), (int(x2)+cols1,int(y2)), (0, 255, 0), 1)
 
     return out
 
+def saveMatchesInImage(img1, img2, matches, imageName):
+    
+    outImg = drawMatches(img1, img2, matches)
+    cv.imwrite(imageName, outImg)
+
 rootPath = "./data_image_stitching/"
 
-image1, image1Gray = loadImages(rootPath, "im1.png")
-image2, image2Gray = loadImages(rootPath, "im2.png")
+# Read images
+image1, image1Gray = loadImages(rootPath, "im89.jpg")
+image2, image2Gray = loadImages(rootPath, "im90.jpg")
 
+# Find keypoints and descriptors
 kpsDsc1 = extractKeypointsAndDescriptors(image1Gray)
 kpsDsc2 = extractKeypointsAndDescriptors(image2Gray)
 
 print("Found keypoints in img1 --> " + str(len(kpsDsc1["keypoints"])))
 print("Found keypoints in img2 --> " + str(len(kpsDsc2["keypoints"])))
 
-matches = matchFeatures(kpsDsc1, kpsDsc2, True)
-# matches = matchFeaturesBuiltin(kpsDsc1["keypoints"], kpsDsc2["keypoints"], kpsDsc1["descriptors"], kpsDsc2["descriptors"])
+# Run brute force matcher to find matched points
+matches = matchFeatures(kpsDsc1, kpsDsc2, False)
+saveMatchesInImage(image1, image2, matches, 'Matches.png')
 
-finalH, inliers = ransac(matches, debug = True)
+# Run RANSAC to eliminate wrong matches and find initial homography
+initialH, inliers = ransac(matches, debug = True)
+# finalH = doGaussianNewton(inliers, initialH)
+saveMatchesInImage(image1, image2, inliers, 'MatchesInliers.png')
 
-# matchImg = warpImages(image1Gray, image2Gray, None)
-# cv.imwrite('Matches.png', matchImg)
+# Warp images according to homography
+warpImages(image1Gray, image2Gray, initialH)
 
-
-print("GAUS NEWTON STARTS")
-
-# error = 0
-# for inlier in bestInliers:
-    
-#     predictedPointForTest = np.matmul(bestH, np.vstack([np.matrix(inlier[0]).T, [1]]))
-#     predictedPointForTest = predictedPointForTest / predictedPointForTest[2]
-#     predictedPointForTest = predictedPointForTest[0:2]
-    
-#     errorDistance = L2Norm(np.array(inlier[1]).T, (predictedPointForTest.T))
-#     error += errorDistance 
-# print("Error --> " + str(error))
-    
-
-# while 500:
-#     jacobian = np.matrix([0,0,0,0,0,0,0,0])
-#     res = np.matrix([0])
-#     for inlier in bestInliers:
-        
-#         sensedPoint = inlier[1]
-        
-#         predictedPoint = np.matmul(bestH, np.vstack([np.matrix(inlier[0]).T, [1]]))
-#         predictedPoint = predictedPoint / predictedPoint[2]
-#         predictedPoint = predictedPoint[0:2]
-        
-#         D = 1/(bestH[2,0] * inlier[0][0] + bestH[2,1] * inlier[0][1] + 1)
-#         jacobian = np.vstack([jacobian , np.multiply(D , [inlier[0][0], inlier[0][1], 1, 0, 0, 0, -inlier[0][0] * predictedPoint[0,0], -inlier[0][1] * predictedPoint[0,0]])])
-#         jacobian = np.vstack([jacobian , np.multiply(D , [0, 0, 0, inlier[0][0], inlier[0][1], 1, -inlier[0][0] * predictedPoint[1,0], -inlier[0][1] * predictedPoint[1,0]])])
-            
-        
-#         res = np.vstack([res , [inlier[1][0] - predictedPoint[0,0] ]])
-#         res = np.vstack([res , [inlier[1][1] - predictedPoint[1,0] ]])
-    
-        
-       
-        
-#     jacobian = jacobian[1:, :]
-#     res = res[1:, :]
-    
-#     deltaH = np.matmul(np.linalg.inv(np.matmul(jacobian.T, jacobian)), np.matmul(jacobian.T, res))
-#     deltaH = np.vstack([deltaH, [1]])
-#     deltaH = deltaH.reshape((3,3))
-     
-#     bestH = bestH + deltaH
-    
-    
-#     error = 0
-#     for inlier in bestInliers:
-        
-#         predictedPointForTest = np.matmul(bestH, np.vstack([np.matrix(inlier[0]).T, [1]]))
-#         predictedPointForTest = predictedPointForTest / predictedPointForTest[2]
-#         predictedPointForTest = predictedPointForTest[0:2]
-        
-#         errorDistance = L2Norm(np.array(inlier[1]).T, (predictedPointForTest.T))
-#         error += errorDistance 
-#     print("Error --> " + str(error))
-
-# dst = cv.warpPerspective(image1,bestH,(image2.shape[1] + image1.shape[1], image2.shape[0]))
-# plt.imshow(dst)
-# plt.show()
-#kullandığın paketlerin version ını ver
-
-# sift için opencv 3.4.1 kullanılma, üst versiyonlarda sift patentli ve kullanılamıyor
